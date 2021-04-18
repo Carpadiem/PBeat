3bc929271ae3b92b291a7a3db65e36d85de99563
Dra4enko <kirillsavelev391@gmail.com>
2021-04-19 01:15:52 +0300

    rrrr
    rrr

---
+++ PBeat/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace PBeat
+{
+    partial class F1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(F1));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Menu = new System.Windows.Forms.Button();
+            this.MusicList = new System.Windows.Forms.Button();
+            this.History = new System.Windows.Forms.Button();
+            this.Playlists = new System.Windows.Forms.Button();
+            this.Settings = new System.Windows.Forms.Button();
+            this.AddMusic = new System.Windows.Forms.Button();
+            this.ProgrammName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(-9, -18);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(824, 68);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(-9, -18);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(79, 486);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Menu
+            // 
+            this.Menu.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Menu.BackgroundImage")));
+            this.Menu.FlatAppearance.BorderSize = 0;
+            this.Menu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Menu.Image = ((System.Drawing.Image)(resources.GetObject("Menu.Image")));
+            this.Menu.Location = new System.Drawing.Point(-9, 48);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(79, 54);
+            this.Menu.TabIndex = 3;
+            this.Menu.UseVisualStyleBackColor = true;
+            // 
+            // MusicList
+            // 
+            this.MusicList.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.MusicList.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MusicList.BackgroundImage")));
+            this.MusicList.FlatAppearance.BorderSize = 0;
+            this.MusicList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MusicList.Image = ((System.Drawing.Image)(resources.GetObject("MusicList.Image")));
+            this.MusicList.Location = new System.Drawing.Point(-5, 102);
+            this.MusicList.Name = "MusicList";
+            this.MusicList.Size = new System.Drawing.Size(75, 48);
+            this.MusicList.TabIndex = 4;
+            this.MusicList.UseVisualStyleBackColor = false;
+            // 
+            // History
+            // 
+            this.History.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("History.BackgroundImage")));
+            this.History.FlatAppearance.BorderSize = 0;
+            this.History.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.History.Image = ((System.Drawing.Image)(resources.GetObject("History.Image")));
+            this.History.Location = new System.Drawing.Point(-5, 151);
+            this.History.Name = "History";
+            this.History.Size = new System.Drawing.Size(75, 48);
+            this.History.TabIndex = 5;
+            this.History.UseVisualStyleBackColor = true;
+            this.History.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Playlists
+            // 
+            this.Playlists.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Playlists.BackgroundImage")));
+            this.Playlists.FlatAppearance.BorderSize = 0;
+            this.Playlists.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Playlists.Image = ((System.Drawing.Image)(resources.GetObject("Playlists.Image")));
+            this.Playlists.Location = new System.Drawing.Point(-5, 199);
+            this.Playlists.Name = "Playlists";
+            this.Playlists.Size = new System.Drawing.Size(75, 48);
+            this.Playlists.TabIndex = 6;
+            this.Playlists.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            this.Settings.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Settings.BackgroundImage")));
+            this.Settings.FlatAppearance.BorderSize = 0;
+            this.Settings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Settings.Image = ((System.Drawing.Image)(resources.GetObject("Settings.Image")));
+            this.Settings.Location = new System.Drawing.Point(-5, 397);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(75, 54);
+            this.Settings.TabIndex = 7;
+            this.Settings.UseVisualStyleBackColor = true;
+            // 
+            // AddMusic
+            // 
+            this.AddMusic.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AddMusic.BackgroundImage")));
+            this.AddMusic.FlatAppearance.BorderSize = 0;
+            this.AddMusic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddMusic.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.AddMusic.Image = ((System.Drawing.Image)(resources.GetObject("AddMusic.Image")));
+            this.AddMusic.Location = new System.Drawing.Point(-5, 247);
+            this.AddMusic.Name = "AddMusic";
+            this.AddMusic.Size = new System.Drawing.Size(75, 42);
+            this.AddMusic.TabIndex = 8;
+            this.AddMusic.UseVisualStyleBackColor = true;
+            // 
+            // ProgrammName
+            // 
+            this.ProgrammName.Font = new System.Drawing.Font("Roboto Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ProgrammName.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.ProgrammName.Image = ((System.Drawing.Image)(resources.GetObject("ProgrammName.Image")));
+            this.ProgrammName.Location = new System.Drawing.Point(394, 9);
+            this.ProgrammName.Name = "ProgrammName";
+            this.ProgrammName.Size = new System.Drawing.Size(59, 19);
+            this.ProgrammName.TabIndex = 9;
+            this.ProgrammName.Text = "PBeat";
+            this.ProgrammName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Roboto", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(104, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 33);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Моя Музыка";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(719, 83);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 11;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(663, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 12;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(607, 83);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 13;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // F1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProgrammName);
+            this.Controls.Add(this.AddMusic);
+            this.Controls.Add(this.Settings);
+            this.Controls.Add(this.Playlists);
+            this.Controls.Add(this.History);
+            this.Controls.Add(this.MusicList);
+            this.Controls.Add(this.Menu);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "F1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button Menu;
+        private System.Windows.Forms.Button MusicList;
+        private System.Windows.Forms.Button History;
+        private System.Windows.Forms.Button Playlists;
+        private System.Windows.Forms.Button Settings;
+        private System.Windows.Forms.Button AddMusic;
+        private System.Windows.Forms.Label ProgrammName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
